52a9e1338bada5b5b37690e7db07f238cdb91676
SanatzhanAimukambetov <sanatzhan.aimukambetov@nu.edu.kz>
2020-12-27 17:16:47 +0600

    label and button were replaced by tableView

--- MVP/MainModule/View/MainViewController.swift
+++ MVP/MainModule/View/MainViewController.swift
@@ -9,25 +9,35 @@ import UIKit
 
 class MainViewController: UIViewController {
     // MARK: - IBOutlet
-    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
     
     var presenter: MainViewPresenterProtocol!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
     }
-    
-    // MARK: - IBAction
-    @IBAction func didTapButtonAction(_ sender: Any) {
-        self.presenter.showGreeting()
-    }
+
 }
 
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Test"
+        return cell
+    }
+    
+    
+}
 extension MainViewController: MainViewProtocol {
     
     func setGreeting(greeting: String) {
-        self.greetingLabel.text = greeting
+
     }
 }
 
